e9b22ea0ce475805d9de8e32240cd0ebd177283c
Angel Alcántara <115179619+AngelUch27@users.noreply.github.com>
2024-06-16 21:30:12 -0600

    Add files via upload

---
+++ PalindromeNumber.py
@@ -0,0 +1,8 @@
+#https://leetcode.com/problems/palindrome-number/description/
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        str_x = str(x)
+        if str_x == str_x[::-1]:
+            return True
+        else:
+            return False       
